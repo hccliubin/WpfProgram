0f69c7223201a2f7bdea2d8e724d8d63b21bd4e6
HeBianGu <908293466@qq.com>
2019-03-15 18:52:13 +0800

    1111

--- Source/Application/WpfControlDemo/View/SliderPage.xaml.cs
+++ Source/Application/WpfControlDemo/View/SliderPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using HeBianGu.General.WpfControlLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -33,6 +35,120 @@ namespace WpfControlDemo.View
             {
                 this.DataContext = _vm;
             };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<IBufferPlayEntity> bufferPlays = new List<IBufferPlayEntity>();
+
+            //  Message：构造1000个测试数据
+            for (int i = 0; i < 1000; i++)
+            {
+                BufferPlayEntity entity = new BufferPlayEntity();
+
+                bufferPlays.Add(entity);
+            }
+
+            //  Message：初始化控件
+            this.control_bufferPlay.MinValue = 0;
+            this.control_bufferPlay.Value = 0;
+            this.control_bufferPlay.BufferValue = 0;
+            this.control_bufferPlay.MaxValue = bufferPlays.Count;
+
+            //  Message：开始缓冲引擎
+            BufferPlayEngine bufferPlayEngine = new BufferPlayEngine(bufferPlays);
+
+            bufferPlayEngine.RefreshCapacity(5);
+
+            bufferPlayEngine.Start();
+
+            Action<bool, int, int> action = (l, k, n) =>
+              {
+                  Application.Current.Dispatcher.Invoke(() =>
+                  {
+                      if (l)
+                      {
+                          this.txt_persent.Text = "缓冲完成..";
+                      }
+                      else
+                      {
+                          string p = (Convert.ToDouble(k) * 100 / Convert.ToDouble(n)).ToString();
+
+                          this.txt_persent.Text = "缓冲中.." + p + "%";
+                      }
+                  });
+
+              };
+
+            //  Message：刷新播放进度
+            Task.Run(() =>
+            {
+                for (int i = 0; i < bufferPlays.Count; i++)
+                {
+                    //  Message：设置当前播放进度值
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        this.control_bufferPlay.Value = i;
+                    });
+
+                    //  Message：检查当前是否已经暂停
+                    while (true)
+                    {
+
+                        bool result = false;
+
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            result = this.btn_play.Content.ToString() == "暂停";
+                        });
+
+                        if (result) break;
+
+                        Thread.Sleep(1000);
+                    }
+
+                    Thread.Sleep(100);
+
+                    //  Message：阻塞等待当前进度是否可以播放
+                    bufferPlayEngine.GetWaitCurrent(l => l == bufferPlays[i], action);
+
+                }
+
+            });
+
+            //  Message：刷新下载进度
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(100);
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        this.control_bufferPlay.BufferValue = bufferPlayEngine.GetBufferSize((int)this.control_bufferPlay.Value);
+                    });
+
+                }
+            });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            button.Content = button.Content.ToString() == "暂停" ? "继续" : "暂停";
+        }
+    }
+
+    public class BufferPlayEntity : BufferPlayEntityBase
+    {
+        public int IsLoaded { get; set; }
+
+        Random random = new Random();
+
+        public override void DoStart()
+        {
+            Thread.Sleep(random.Next(1, 2) * 1000);
         }
     }
 
